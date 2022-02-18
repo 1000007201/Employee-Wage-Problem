d2d958d3fe9035fe3517f7813d46bdaa24c2e8cd
1000007201 <nishant.sharma5504@gmail.com>
2022-02-18 14:55:32 +0530

    [Nishant] Add:Max Working Hour Added

--- Employee/main.py
+++ Employee/main.py
@@ -5,6 +5,7 @@ import random
 # Constants
 WAGE_PER_HR = 20
 WR_DAY_PER_MONTH = 20
+MAX_WRK_HR = 100
 
 
 switcher = {
@@ -16,9 +17,13 @@ switcher = {
 
 def calculate_total_wage():
     total_wage = 0
-    for i in range(0, WR_DAY_PER_MONTH):
+    temp_day = WR_DAY_PER_MONTH
+    temp_hr = MAX_WRK_HR
+    while temp_day > 0 and temp_hr > 0:
         attendance = random.randint(0, 2)
         wage = switcher.get(attendance).get("Working_Hr") * WAGE_PER_HR
+        temp_hr -= switcher.get(attendance).get("Working_Hr")
+        temp_day -= 1
         total_wage += wage
     return total_wage
 
