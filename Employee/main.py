af03856396372d65fa02e1a29d06d9676e463115
1000007201 <nishant.sharma5504@gmail.com>
2022-02-18 14:31:55 +0530

    [Nishant] Add:Monthly wage calculated

--- Employee/main.py
+++ Employee/main.py
@@ -1,19 +1,27 @@
 # Imports:
 import random
 
+
 # Constants
 WAGE_PER_HR = 20
+WR_DAY_PER_MONTH = 20
 
-attendance = random.randint(0, 2)
-# 0-->ABSENT
-# 1-->PRESENT
-# 2-->PART_TIME
 
 switcher = {
-    0:{"Status": "ABSENT","Working_Hr": 0},
-    1:{"Status": "PRESENT","Working_Hr": 12},
-    2:{"Status": "PART_TIME","Working_Hr": 8}
+    0: {"Status": "ABSENT", "Working_Hr": 0},
+    1: {"Status": "PRESENT", "Working_Hr": 12},
+    2: {"Status": "PART_TIME", "Working_Hr": 8}
 }
-wage = switcher.get(attendance).get("Working_Hr") * WAGE_PER_HR
-print(f"{wage} is Daily Wage")
+
+
+def calculate_total_wage():
+    total_wage = 0
+    for i in range(0, WR_DAY_PER_MONTH):
+        attendance = random.randint(0, 2)
+        wage = switcher.get(attendance).get("Working_Hr") * WAGE_PER_HR
+        total_wage += wage
+    return total_wage
+
+
+print(f"{calculate_total_wage()} is Monthly Wage")
 
