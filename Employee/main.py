585d9126f6d84073419bef10c7be3a7dd9df046b
1000007201 <nishant.sharma5504@gmail.com>
2022-02-18 13:53:18 +0530

    [Nishant] Add:Check Present or Absent

--- Employee/main.py
+++ Employee/main.py
@@ -1,1 +1,15 @@
-print("Welcome To Employee Wage Computation")
+# Imports:
+import random
+
+# Constants
+PRESENT = 1
+ABSENT = 0
+
+attendence = random.randint(0,1)
+
+switcher = {
+    0:{"Status":"ABSENT"},
+    1:{"Status":"PRESENT"}
+}
+
+print(switcher.get(attendence).get("Status"))
